23526f039d82c03956ef67dd62d8b7d71db0618b
Dilshan09 <pereradilshan594@gmail.com>
2024-02-01 19:24:01 +0530

    ReturnTest added

---
+++ tests/ReturnTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ReturnTest extends TestCase
+{
+    public function testTitleValidation()
+    {
+        $title = "Pixzarloop";
+
+        if ($title !== null) {
+            $result = "Title is a not null : $title";
+        } else {
+            $result = "Title is null ";
+        }
+
+        // Output custom message
+        echo $result;
+
+        // Assert that the result is as expected
+        $this->assertTrue($title !== null , $result);
+    }
+}
